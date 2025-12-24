fea93bca193eee73b92a00ea290ccd0f21c0fbb4
blissexodia <binnoley@gmail.com>
2025-12-24 10:26:19 +0545

    work

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,21 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  //Database name
+  static const String dbeName = 'lost_n_found_db';
+
+  static const int batchTypeId = 0;
+  static const String batchTable = 'batch_table';
+
+  static const int studentTypeId = 1;
+  static const String studentTable = 'student_table';
+
+  static const int itemTypeId = 2;
+  static const String itemTable = 'item_table';
+
+  static const int categoryTypeId = 3;
+  static const String categoryTable = 'category_table';
+
+  static const int commentsTypesId = 4;
+  static const String commentsTable = 'comments_table';
+}
