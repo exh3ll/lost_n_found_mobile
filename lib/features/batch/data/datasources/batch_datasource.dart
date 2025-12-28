e7124a140cf8f8be2688c9b0b5b79dde6a9a735b
blissexodia <binnoley@gmail.com>
2025-12-28 12:53:03 +0545

    Add IBatchDatasource interface for batch data operations

--- lib/features/batch/data/datasources/batch_datasource.dart
+++ lib/features/batch/data/datasources/batch_datasource.dart
@@ -1,12 +1,9 @@
-import 'package:either_dart/either.dart';
-import 'package:lost_n_found/core/error/failures.dart';
 import 'package:lost_n_found/features/batch/data/models/batch_hive_model.dart';
-import 'package:lost_n_found/features/batch/domain/entities/batch_entity.dart';
 
 abstract interface class IBatchDatasource {
   Future<List<BatchHiveModel>> getAllBatches();
-  Future<BatchEntity> getBatchById(String, batchId);
-  Future<bool> createBatch(BatchEntity batch);
-  Future<bool> upadteBatch(BatchEntity batch);
-  Future<bool> deletebatch(String batchId);
+  Future<BatchHiveModel?> getBatchById(String batchId);
+  Future<bool> createBatch(BatchHiveModel batch);
+  Future<bool> updateBatch(BatchHiveModel batch);
+  Future<bool> deleteBatch(String batchId);
 }
