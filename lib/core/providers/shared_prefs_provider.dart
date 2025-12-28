5aa96d221fefcca44384dfffbe876e2eae34b0f4
blissexodia <binnoley@gmail.com>
2025-12-28 12:52:39 +0545

    Add Hive service provider to core providers

--- lib/core/providers/shared_prefs_provider.dart
+++ lib/core/providers/shared_prefs_provider.dart
@@ -1,10 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../services/storage/storage_service.dart';
+import '../services/hive/hive_service.dart';
 
 // ====================== SHARED PREFERENCES PROVIDER ======================
 
 /// Must be overridden in main.dart before runApp()
 final storageServiceProvider = Provider<StorageService>((ref) {
   throw UnimplementedError('storageServiceProvider must be overridden');
+});
+
+// ====================== HIVE SERVICE PROVIDER ======================
+
+/// Must be overridden in main.dart before runApp()
+final hiveServiceProvider = Provider<HiveService>((ref) {
+  throw UnimplementedError('hiveServiceProvider must be overridden');
 });
